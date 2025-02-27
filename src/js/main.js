e54bbf9c1d0119c62c34cfbc75bbdf0f40e3f441
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2025-02-27 10:09:04 -0700

    loading images form a json file

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import { getParkData } from "./parkService.mjs";
 
-const parkData = getParkData();
+
+async function bannerImage() {
+    const parkData = getParkData();
+
+    const bannerImage = document.getElementById("hero-banner__image");
+
+    bannerImage.src = parkData.images[0].url;
+}
+
+bannerImage();
