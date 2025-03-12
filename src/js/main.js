8c699de0e9a361163bb0223b707a84a179e4990a
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2025-03-12 04:57:12 -0600

    debugging

--- src/js/main.js
+++ src/js/main.js
@@ -12,28 +12,6 @@ async function bannerImage() {
 const parkData = await getParkData();
 
 
-const parkInfoLinks = [
-  {
-    name: "Current Conditions &#x203A;",
-    link: "conditions.html",
-    image: parkData.images[2].url,
-    description:
-      "See what conditions to expect in the park before leaving on your trip!"
-  },
-  {
-    name: "Fees and Passes &#x203A;",
-    link: "fees.html",
-    image: parkData.images[3].url,
-    description: "Learn about the fees and passes that are available."
-  },
-  {
-    name: "Visitor Centers &#x203A;",
-    link: "visitor_centers.html",
-    image: parkData.images[9].url,
-    description: "Learn about the visitor centers in the park."
-  }
-];
-
 function mainHeader(){
   bannerImage();
   const mainTitle = document.getElementById("mainTitle");
