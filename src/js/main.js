11f5fe43ab5dc70706cae61555c0164d188c4fad
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2025-03-12 04:52:52 -0600

    footer maker

--- src/js/main.js
+++ src/js/main.js
@@ -9,39 +9,87 @@ async function bannerImage() {
     bannerImage.src = parkData.images[0].url;
 }
 
-bannerImage();
+const parkData = await getParkData();
+
 
 const parkInfoLinks = [
-    {
-      name: "Current Conditions &#x203A;",
-      link: "conditions.html",
-      image: parkData.images[2].url,
-      description:
-        "See what conditions to expect in the park before leaving on your trip!"
-    },
-    {
-      name: "Fees and Passes &#x203A;",
-      link: "fees.html",
-      image: parkData.images[3].url,
-      description: "Learn about the fees and passes that are available."
-    },
-    {
-      name: "Visitor Centers &#x203A;",
-      link: "visitor_centers.html",
-      image: parkData.images[9].url,
-      description: "Learn about the visitor centers in the park."
-    }
-  ];
+  {
+    name: "Current Conditions &#x203A;",
+    link: "conditions.html",
+    image: parkData.images[2].url,
+    description:
+      "See what conditions to expect in the park before leaving on your trip!"
+  },
+  {
+    name: "Fees and Passes &#x203A;",
+    link: "fees.html",
+    image: parkData.images[3].url,
+    description: "Learn about the fees and passes that are available."
+  },
+  {
+    name: "Visitor Centers &#x203A;",
+    link: "visitor_centers.html",
+    image: parkData.images[9].url,
+    description: "Learn about the visitor centers in the park."
+  }
+];
 
-async  function populateMain(data) {
-    const card = document.querySelector("head> title").textContent = 
-    document.querySelector("head > title").textContent = data.fullName;
-    // set the banner image
-    document.querySelector(".hero-banner > img").src = data.images[0].url;
-    // use the template function above to set the rest of the park specific info in the header
-    document.querySelector(".hero-banner__content").innerHTML =
-      parkInfoTemplate(data);
+function mainHeader(){
+  bannerImage();
+  const mainTitle = document.getElementById("mainTitle");
+  const div = document.createElement("div");
+  div.classList.add("main-header");
+  div.innerHTML = `<h1 class = "main-park-name">${parkData.fullName}</h1>
+                  <p class = "park-description">${parkData.description}</p>`;
+
+  mainTitle.appendChild(div);
 }
 
-populateMain();
+function mainHolder(data){
+  mainHeader();
+  for( let i = 0; i < data.length; i++){
+    const main = document.querySelector("main");
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `<img class = "card-image" src="${data[i].image}" alt="${data[i].name}">
+                    <a href="${data[i].link}"> <h2 class = "card-title">${data[i].name}</h2></a>
+                    <p class = "card-description">${data[i].description}</p>`;
+    main.appendChild(div);
+  }
+}
+
+mainHolder(parkInfoLinks);
+
+function makeFooter(){
+  const footer = document.querySelector("footer");
+  let contact = document.createElement("h3");
+  contact.classList.add("contact");
+  contact.textContent = "Contact info";
+  footer.appendChild(contact);
+
+  let postalCode = parkData.addresses[1].postalCode;
+  let city = parkData.addresses[1].city;
+  let stateCode = parkData.addresses[1].stateCode;
+  let line1 =  parkData.addresses[1].line1;
+  let phone = parkData.contacts.phoneNumbers[0].phoneNumber;
+
+  let div1 = document.createElement("div");
+  div1.innerHTML = `<h3>Mailing Address</h3>
+                    <p>${line1}</p>
+                    <p>${city} ${stateCode} ${postalCode}</p>`;
+  let div2 = document.createElement("div");
+  div2.innerHTML = `<h3>Phone</h3>
+                    <p> ${phone}</p>`;
+
+  footer.appendChild(div1);
+  footer.appendChild(div2);
+
+
+}
+
+makeFooter();
+
+
+
+
 
---
+++ src/week2/fetch.js
@@ -0,0 +1,55 @@
+// fetch.js
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon"
+let results = null;
+async function getPokemon(url) {
+  const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuff(data);
+  }
+}
+function doStuff(data) {
+  const element = document.querySelector("section");
+  results = data;
+  const html = `<h2>${results.name}</h2>
+                <img src="${results.sprites.front_default}" alt="Image of ${results.name}">`;
+element.innerHTML = html;
+console.log("first: ", results);
+}
+
+function doStuffList(data) {
+  console.log(data);
+  const pokeListElement = document.querySelector("#outputList");
+  const pokeList = data.results;
+  pokeList.forEach((currentItem) => {
+    const html = `<li>${currentItem.name}</li>`;
+    // note the += here...
+    pokeListElement.innerHTML += html;
+  });
+}
+
+async function getPokemonList(url){
+  const response = await fetch(url);
+
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuffList(data);
+  }
+
+}
+
+getPokemon(url);
+getPokemonList(urlList);
+console.log("second: ", results);
+
+
+// function doStuffCesar(){
+//   const sec = document.querySelector("section");
+
+// }
