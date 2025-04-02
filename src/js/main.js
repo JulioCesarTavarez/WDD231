7567be53c4b71b305d89ca407485937db9c44987
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2025-04-02 15:51:03 -0600

    Finishing the weekly challenge

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,81 @@ function toggleMenu() {
       menuButtonIcon.classList.add("hide");
   }
 }
+const planVisit = document.getElementById("planVisit");
+const planVisit2 = document.getElementById("planVisit2");
+const learnExplore = document.getElementById("learnExplore");
+const getInvolved = document.getElementById("getInvolved");
+let planVisitValue = false;
+let planVisit2Value = false;
+let learnExploreValue = false;
+let getInvolvedValue = false;
+
+planVisit.addEventListener("click", togglePlanVisit);
+planVisit2.addEventListener("click", togglePlanVisit2);
+learnExplore.addEventListener("click", toggleLearnExplore);
+getInvolved.addEventListener("click", toggleGetInvolved);
+
+function togglePlanVisit() {
+  planVisitValue = !planVisitValue;
+  const planVisitList = document.getElementById("planVisitList");
+  let arrow = document.getElementById("planVisitArrow");
+  if (planVisitValue) {
+      planVisitList.classList.add("open");
+      planVisitList.classList.remove("close");
+      arrow.style.transform = "rotate(180deg)";
+  } else {
+      planVisitList.classList.remove("open");
+      planVisitList.classList.add("close");
+      arrow.style.transform = "rotate(0deg)";
+  }
+}
+function togglePlanVisit2() {
+  planVisit2Value = !planVisit2Value;
+  const planVisit2List = document.getElementById("planVisit2List");
+  let arrow = document.getElementById("planVisit2Arrow");
+  if (planVisit2Value) {
+      planVisit2List.classList.add("open");
+      planVisit2List.classList.remove("close");
+      arrow.style.transform = "rotate(180deg)";
+  } else {
+      planVisit2List.classList.remove("open");
+      planVisit2List.classList.add("close");
+      arrow.style.transform = "rotate(0deg)";
+  }
+}
+
+function toggleLearnExplore() {
+  learnExploreValue = !learnExploreValue;
+  let arrow = document.getElementById("learnExploreArrow");
+  const learnExploreList = document.getElementById("learnExploreList");
+  if (learnExploreValue) {
+      learnExploreList.classList.add("open");
+      learnExploreList.classList.remove("close");
+      arrow.style.transform = "rotate(180deg)";
+  } else {
+      learnExploreList.classList.remove("open");
+      learnExploreList.classList.add("close");
+      arrow.style.transform = "rotate(0deg)";
+  }
+}
+
+function toggleGetInvolved() {
+  getInvolvedValue = !getInvolvedValue;
+  const getInvolvedList = document.getElementById("getInvolvedList");
+  let arrow = document.getElementById("getInvolvedArrow");
+  if (getInvolvedValue) {
+      getInvolvedList.classList.add("open");
+      getInvolvedList.classList.remove("close");
+      arrow.style.transform = "rotate(180deg)";
+  } else {
+      getInvolvedList.classList.remove("open");
+      getInvolvedList.classList.add("close");
+      arrow.style.transform = "rotate(0deg)";
+  }
+}
+
+
+
 
 
 
